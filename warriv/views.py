2225928930d4e220bc7ec7611c7f89b667221a02
Jason Harvey <alienth@gmail.com>
2013-05-04 23:16:38 -0700

    new views

--- warriv/views.py
+++ warriv/views.py
@@ -1,36 +1,60 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 
+from formencode import Schema, validators
+from pyramid_simpleform import Form
+
+import logging
+
+log = logging.getLogger(__name__)
+
+
 from sqlalchemy.exc import DBAPIError
 
 from .models import (
     DBSession,
     Account,
+    Hero,
+    Ladder,
     )
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
+class AccountSchema(Schema):
+
+    username = validators.PlainText(max=20, min=3)
+    password = validators.UnicodeString(max=40, min=5)
+
+@view_config(route_name='front', renderer='templates/front.pt')
+def front(request):
     pass
 #    try:
 #        one = DBSession.query(Account).filter(Account.name == 'one').first()
 #    except DBAPIError:
 #        return Response(conn_err_msg, content_type='text/plain', status_int=500)
 #    return {'one': one, 'project': 'warriv'}
+    return {}
 
-conn_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
 
-1.  You may need to run the "initialize_warriv_db" script
-    to initialize your database tables.  Check your virtual 
-    environment's "bin" directory for this script and try to run it.
+@view_config(route_name='api_register', renderer='templates/front.pt')
+def register(request):
 
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
+    form = Form(request, schema=AccountSchema())
 
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+    if form.validate():
+
+      if Account.by_username(form.data['username']):
+          form.errors = 'username already taken'
+          log.info(form.errors)
+          return {}
+
+      account = form.bind(Account())
+
+      DBSession.add(account)
+
+      return {}
+
+
+    log.info(form.errors)
+
+    return {}
 
