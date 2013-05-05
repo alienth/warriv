b67c019786868d21d6c5313becd9d3bc7dba5cfa
Jason Harvey <alienth@gmail.com>
2013-05-04 23:16:21 -0700

    new classes for Hero and Ladder

--- warriv/models.py
+++ warriv/models.py
@@ -28,3 +28,55 @@ class Account(Base):
         self.username = username
         self.password = password
         self.oauth_token = oauth_token
+
+    @classmethod
+    def by_username(cls, username):
+        return DBSession.query(cls).filter(cls.username == username).first()
+
+
+class Hero(Base):
+    __tablename__ = 'hero'
+    id = Column(Integer, primary_key=True)
+    account_id = Column(Integer)
+    name = Column(Text)
+    bt = Column(Text)
+    ladder = Column(Integer)
+
+    def __init__(self, account_id='', name='', bt='', ladder=''):
+        self.account_id = account_id
+        self.name = name
+        self.bt = bt
+        self.ladder = ladder
+
+    def set_ladder(self, ladder):
+        self.ladder = ladder
+
+class Ladder(Base):
+    __tablename__ = 'ladder'
+    id = Column(Integer, primary_key=True)
+    season = Column(Integer)
+    name = Column(Text)
+    type = Column(Text)
+    status = Column(Integer)
+
+    # status constants
+    ACTIVE    = 0
+    PENDING   = 1
+    COMPLETED = 2
+
+    # type constants
+    SOFTCORE = 0
+    HARDCORE = 1
+
+    def __init__(self, season='', name='', type=SOFTCORE, status=PENDING):
+        self.season = season
+        self.name = name
+        self.type = type
+        self.status = status
+
+    @classmethod
+    def get_all(cls):
+        return DBSession.query(cls).all()
+
+
+
